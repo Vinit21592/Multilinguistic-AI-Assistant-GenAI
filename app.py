0d6bb62d2a43d249c98d011e0fd3bbb1b97b2287
vinit <vinit.londhe21@gmail.com>
2024-04-15 20:20:49 +0530

    app added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src.helper import voice_input, llm_model_object, text_to_speech
 
 
 def main():
-    st.title("Multilingual AI Assistant 💁")
+    st.title("Multilingual AI Assistant 💁🤖")
 
     if st.button("Ask me anything"):
         with st.spinner("Listening..."):
@@ -14,9 +14,12 @@ def main():
             audio_file=open("speech.mp3", "rb")
             audio_bytes=audio_file.read()
 
-            st.text_area()
-            st.audio()
-            st.download_button()
+            st.text_area(label="Response:",value=response,height=350)
+            st.audio(audio_bytes)
+            st.download_button(label="Download Speech",
+                               data=audio_bytes,
+                               file_name="speech.mp3",
+                               mime="audio/mp3")
 
 if __name__ == "__main__":
     main()
